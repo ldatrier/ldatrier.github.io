279cbc0d3c8da159d7c7e7007331562817febd69
Hunter <hunter.gabbard@gmail.com>
2019-10-31 15:10:09 +0000

    Added Hattiesburg talk location

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,11 +5,6 @@ var addressPoints = [
     -122.2728639
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276474
-  ],
-  [
     "Los Angeles, CA",
     34.0536909,
     -118.2427666
@@ -18,5 +13,10 @@ var addressPoints = [
     "Baltimore, Maryland",
     39.2908816,
     -76.610759
+  ],
+  [
+    "Hattiesburg, MS",
+    31.3271189,
+    -89.2903392
   ]
 ];
